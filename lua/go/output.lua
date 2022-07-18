940228eaa46f91ff86d7398469a8f5a0a850b530
朱熙 <zhuxi@bilibili.com>
2022-07-18 13:33:03 +0800

    测试用例窗口宽度

--- lua/go/output.lua
+++ lua/go/output.lua
@@ -113,7 +113,7 @@ function M.popup_job_result(results, opts)
         border = { 1, 1, 1, 1 },
         cursorline = true,
         maxheight = pos.height,
-        minwidth = vim.api.nvim_win_get_width(0)-4, -- 保持屏幕宽度
+        minwidth = vim.o.columns - 4, -- 保持屏幕宽度
         width = pos.width,
 	padding = {0, 0, 0, 0},
         highlight = 'GoTestResult',
