27cd191c792611d88ea0e7c1f2690dde6c4a57fc
朱熙 <zhuxi@bilibili.com>
2023-04-03 23:06:46 +0800

    dap-go

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -288,32 +288,32 @@ return require('packer').startup(function()
 	use {
 		'leoluz/nvim-dap-go',
 		config = function()
-			local dap = require('dap')
-			dap.setup {
-				-- Additional dap configurations can be added.
-				-- dap_configurations accepts a list of tables where each entry
-				-- represents a dap configuration. For more details do:
-				-- :help dap-configuration
-				dap_configurations = {
-					{
-						-- Must be "go" or it will be ignored by the plugin
-						type = "go",
-						name = "Attach remote",
-						mode = "remote",
-						request = "attach",
-					},
-				},
-				-- delve configurations
-				delve = {
-					-- time to wait for delve to initialize the debug session.
-					-- default to 20 seconds
-					initialize_timeout_sec = 20,
-					-- a string that defines the port to start delve debugger.
-					-- default to string "${port}" which instructs nvim-dap
-					-- to start the process in a random available port
-					port = "${port}"
-				},
-			}
+			-- local dap = require('dap-go')
+			-- dap.setup {
+			-- 	-- Additional dap configurations can be added.
+			-- 	-- dap_configurations accepts a list of tables where each entry
+			-- 	-- represents a dap configuration. For more details do:
+			-- 	-- :help dap-configuration
+			-- 	dap_configurations = {
+			-- 		{
+			-- 			-- Must be "go" or it will be ignored by the plugin
+			-- 			type = "go",
+			-- 			name = "Attach remote",
+			-- 			mode = "remote",
+			-- 			request = "attach",
+			-- 		},
+			-- 	},
+			-- 	-- delve configurations
+			-- 	delve = {
+			-- 		-- time to wait for delve to initialize the debug session.
+			-- 		-- default to 20 seconds
+			-- 		initialize_timeout_sec = 20,
+			-- 		-- a string that defines the port to start delve debugger.
+			-- 		-- default to string "${port}" which instructs nvim-dap
+			-- 		-- to start the process in a random available port
+			-- 		port = "${port}"
+			-- 	},
+			-- }
 
 			-- dap.adapters.go = {
 			-- 	type = 'executable',
