67935dcf29a289dc050bd5548443dcbe40cdbec0
朱熙 <zhuxi@bilibili.com>
2024-05-24 14:13:45 +0800

    兼容 neovim 0.10

--- vim.lua
+++ vim.lua
@@ -191,7 +191,8 @@ if vim.version().minor >= 10 then
 		callback = function(args)
 			local client = vim.lsp.get_client_by_id(args.data.client_id)
 			if client.server_capabilities.inlayHintProvider then
-				vim.lsp.inlay_hint(args.buf, true)
+				-- vim.lsp.inlay_hint(args.buf, true)
+				vim.lsp.inlay_hint.enable(true)
 			end
 			-- whatever other lsp config you want
 		end
