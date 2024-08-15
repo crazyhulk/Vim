628dd1920196b9428d77b8084ea11d63e6a1af4a
朱熙 <zhuxi@bilibili.com>
2024-08-15 14:55:16 +0800

    add markdown

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -419,4 +419,18 @@ return require('packer').startup(function()
 	use { "catppuccin/nvim", as = "catppuccin" } -- theme
 	use { 'sainnhe/gruvbox-material' }
 
+	use {
+		"OXY2DEV/markview.nvim",
+		lazy = false,      -- Recommended
+		-- ft = "markdown" -- If you decide to lazy-load anyway
+
+		dependencies = {
+			-- You will not need this if you installed the
+			-- parsers manually
+			-- Or if the parsers are in your $RUNTIMEPATH
+			"nvim-treesitter/nvim-treesitter",
+
+			"nvim-tree/nvim-web-devicons"
+		}
+	}
 end)
