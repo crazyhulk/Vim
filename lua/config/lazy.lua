f305f88067d1026d79dd8f7d732ba0b3c93ad6ad
朱熙 <zhuxi@bilibili.com>
2025-12-16 19:33:00 +0800

    取消 avante 插件

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -506,116 +506,116 @@ require("lazy").setup({
 				-- words = { enabled = true },
 			}
 		},
-		{
-			"yetone/avante.nvim",
-			event = "VeryLazy",
-			lazy = false,
-			-- enabled = false,
-			version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
-			opts = {
-				provider = "copilot",
-				-- provider = "claude", -- The provider to use for Avante, can be 'openai', 'deepseek', 'copilot', 'claude', etc.
-				auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
-
-				providers = {
-					deepseek = {
-						__inherited_from = 'openai',
-						api_key_name = "OPENAI_API_KEY",
-						endpoint = "https://api.deepseek.com",
-						model = "deepseek-coder",
-					},
-					---@type AvanteSupportedProvider
-					openai = {
-						api_key_name = "OPENAI_API_KEY",
-						endpoint = "https://api.deepseek.com/v1",
-						-- model = "deepseek-chat",
-						model = "deepseek-coder",
-						timeout = 30000, -- Timeout in milliseconds
-						extra_request_body = {
-							temperature = 0,
-							max_tokens = 4096,
-						}
-					},
-					copilot = {
-						endpoint = "https://api.githubcopilot.com",
-						model = "claude-3.7-sonnet",
-						-- model = "claude-sonnet-4",
-						-- model = "gpt-4o-2024-08-06",
-						allow_insecure = false, -- Allow insecure server connections
-						timeout = 30000, -- Timeout in milliseconds
-						extra_request_body = {
-							temperature = 0,
-							max_tokens = 8192,
-						}
-					},
-					claude = {
-						-- endpoint = "https://api.anthropic.com",
-						endpoint = "https://api.oaipro.com/",
-						model = "claude-sonnet-4-20250514",
-						timeout = 30000, -- Timeout in milliseconds
-						extra_request_body = {
-							temperature = 0.75,
-							max_tokens = 20480,
-						},
-					},
-				}
-			},
-			-- opts = {
-			-- 	-- add any opts here
-			-- 	provider = "copilot",
-			-- 	model = "Claude 3.5 Sonnet",
-			-- 	copilot = {
-			-- 		endpoint = "https://api.githubcopilot.com",
-			-- 		model = "claude-3.5-sonnet",
-			-- 		-- model = "gpt-4o-2024-08-06",
-			-- 		proxy = nil, -- [protocol://]host[:port] Use this proxy
-			-- 		allow_insecure = false, -- Allow insecure server connections
-			-- 		timeout = 30000, -- Timeout in milliseconds
-			-- 		temperature = 0,
-			-- 		max_tokens = 4096,
-			-- 	},
-			-- },
-			-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-			build = "make BUILD_FROM_SOURCE=true",
-			-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-			dependencies = {
-				"folke/snacks.nvim",
-				"nvim-lua/plenary.nvim",
-				"MunifTanjim/nui.nvim",
-				--- The below dependencies are optional,
-				"echasnovski/mini.pick", -- for file_selector provider mini.pick
-				"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-				-- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-				"ibhagwan/fzf-lua", -- for file_selector provider fzf
-				"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-				"zbirenbaum/copilot.lua", -- for providers='copilot'
-				{
-					-- support for image pasting
-					"HakonHarnes/img-clip.nvim",
-					event = "VeryLazy",
-					opts = {
-						-- recommended settings
-						default = {
-							embed_image_as_base64 = false,
-							prompt_for_file_name = false,
-							drag_and_drop = {
-								insert_mode = true,
-							},
-							-- required for Windows users
-							use_absolute_path = true,
-						},
-					},
-				},
-				{
-					-- Make sure to set this up properly if you have lazy=true
-					'MeanderingProgrammer/render-markdown.nvim',
-					opts = {
-						file_types = { "markdown", "Avante" },
-					},
-					ft = { "markdown", "Avante" },
-				},
-			},
-		},
+		-- {
+		-- 	"yetone/avante.nvim",
+		-- 	event = "VeryLazy",
+		-- 	lazy = false,
+		-- 	-- enabled = false,
+		-- 	version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+		-- 	opts = {
+		-- 		provider = "copilot",
+		-- 		-- provider = "claude", -- The provider to use for Avante, can be 'openai', 'deepseek', 'copilot', 'claude', etc.
+		-- 		auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+                --
+		-- 		providers = {
+		-- 			deepseek = {
+		-- 				__inherited_from = 'openai',
+		-- 				api_key_name = "OPENAI_API_KEY",
+		-- 				endpoint = "https://api.deepseek.com",
+		-- 				model = "deepseek-coder",
+		-- 			},
+		-- 			---@type AvanteSupportedProvider
+		-- 			openai = {
+		-- 				api_key_name = "OPENAI_API_KEY",
+		-- 				endpoint = "https://api.deepseek.com/v1",
+		-- 				-- model = "deepseek-chat",
+		-- 				model = "deepseek-coder",
+		-- 				timeout = 30000, -- Timeout in milliseconds
+		-- 				extra_request_body = {
+		-- 					temperature = 0,
+		-- 					max_tokens = 4096,
+		-- 				}
+		-- 			},
+		-- 			copilot = {
+		-- 				endpoint = "https://api.githubcopilot.com",
+		-- 				model = "claude-3.7-sonnet",
+		-- 				-- model = "claude-sonnet-4",
+		-- 				-- model = "gpt-4o-2024-08-06",
+		-- 				allow_insecure = false, -- Allow insecure server connections
+		-- 				timeout = 30000, -- Timeout in milliseconds
+		-- 				extra_request_body = {
+		-- 					temperature = 0,
+		-- 					max_tokens = 8192,
+		-- 				}
+		-- 			},
+		-- 			claude = {
+		-- 				-- endpoint = "https://api.anthropic.com",
+		-- 				endpoint = "https://api.oaipro.com/",
+		-- 				model = "claude-sonnet-4-20250514",
+		-- 				timeout = 30000, -- Timeout in milliseconds
+		-- 				extra_request_body = {
+		-- 					temperature = 0.75,
+		-- 					max_tokens = 20480,
+		-- 				},
+		-- 			},
+		-- 		}
+		-- 	},
+		-- 	-- opts = {
+		-- 	-- 	-- add any opts here
+		-- 	-- 	provider = "copilot",
+		-- 	-- 	model = "Claude 3.5 Sonnet",
+		-- 	-- 	copilot = {
+		-- 	-- 		endpoint = "https://api.githubcopilot.com",
+		-- 	-- 		model = "claude-3.5-sonnet",
+		-- 	-- 		-- model = "gpt-4o-2024-08-06",
+		-- 	-- 		proxy = nil, -- [protocol://]host[:port] Use this proxy
+		-- 	-- 		allow_insecure = false, -- Allow insecure server connections
+		-- 	-- 		timeout = 30000, -- Timeout in milliseconds
+		-- 	-- 		temperature = 0,
+		-- 	-- 		max_tokens = 4096,
+		-- 	-- 	},
+		-- 	-- },
+		-- 	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		-- 	build = "make BUILD_FROM_SOURCE=true",
+		-- 	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+		-- 	dependencies = {
+		-- 		"folke/snacks.nvim",
+		-- 		"nvim-lua/plenary.nvim",
+		-- 		"MunifTanjim/nui.nvim",
+		-- 		--- The below dependencies are optional,
+		-- 		"echasnovski/mini.pick", -- for file_selector provider mini.pick
+		-- 		"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+		-- 		-- "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+		-- 		"ibhagwan/fzf-lua", -- for file_selector provider fzf
+		-- 		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+		-- 		"zbirenbaum/copilot.lua", -- for providers='copilot'
+		-- 		{
+		-- 			-- support for image pasting
+		-- 			"HakonHarnes/img-clip.nvim",
+		-- 			event = "VeryLazy",
+		-- 			opts = {
+		-- 				-- recommended settings
+		-- 				default = {
+		-- 					embed_image_as_base64 = false,
+		-- 					prompt_for_file_name = false,
+		-- 					drag_and_drop = {
+		-- 						insert_mode = true,
+		-- 					},
+		-- 					-- required for Windows users
+		-- 					use_absolute_path = true,
+		-- 				},
+		-- 			},
+		-- 		},
+		-- 		{
+		-- 			-- Make sure to set this up properly if you have lazy=true
+		-- 			'MeanderingProgrammer/render-markdown.nvim',
+		-- 			opts = {
+		-- 				file_types = { "markdown", "Avante" },
+		-- 			},
+		-- 			ft = { "markdown", "Avante" },
+		-- 		},
+		-- 	},
+		-- },
 		{
 			"jinzhongjia/LspUI.nvim",
 			branch = "main",
